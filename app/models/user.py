08fc4402840956f1fa141db689a84e38e4a95189
Linxd2015 <364026456@qq,com>
2018-07-09 00:20:19 +0800

    Flask done

--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,22 @@
+from math import floor
+
 from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+from app.libs.enums import PendingStatus
 from app.libs.helper import is_isbn_or_key
 from app.models.base import Base, db
 from sqlalchemy import Column, Integer, String, Boolean, Float
 from flask_login import UserMixin
 from app import login_manager
+from app.models.drift import Drift
 from app.models.gift import Gift
 from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
 
 
-class User(UserMixin,Base):
+class User(UserMixin, Base):
     id = Column(Integer, primary_key=True)
     nickname = Column(String(24), nullable=False)
     phone_number = Column(String(18), unique=True)
@@ -32,6 +36,17 @@ class User(UserMixin,Base):
     @password.setter
     def password(self, raw):
         self._password = generate_password_hash(raw)
+
+    def can_send_drift(self):
+        if self.beans < 1:
+            return False
+        success_gifts_count = Gift.query.filter_by(
+            uid=self.id, launched=True).count()
+        success_receive_count = Drift.query.filter_by(
+            requester_id=self.id, pending=PendingStatus.Success).count()
+
+        print(self.beans)
+        return True if floor(success_receive_count / 2) <= floor(success_gifts_count) else False
 
     def check_password(self, raw):
         return check_password_hash(self._password, raw)
@@ -55,7 +70,7 @@ class User(UserMixin,Base):
 
     def generate_token(self, expiration=600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'id':self.id}).decode('utf-8')
+        return s.dumps({'id': self.id}).decode('utf-8')
 
     @staticmethod
     def reset_password(token, new_password):
@@ -69,6 +84,16 @@ class User(UserMixin,Base):
             user = User.query.get(uid)
             user.password = new_password
         return True
+
+    @property
+    def summary(self):
+        return dict(
+            nickname=self.nickname,
+            beans=self.beans,
+            email=self.email,
+            send_receive=str(self.send_counter) + '/' + str(self.receive_counter)
+        )
+
 
 @login_manager.user_loader
 def get_user(uid):
